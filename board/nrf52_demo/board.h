aa90646e1ed36f0249977b6c1c6f020c713330e5
Roman Luitko <dossalab@pm.me>
2020-10-23 23:57:28 +0300

    remove unnecessary servo macro

--- board/nrf52_demo/board.h
+++ board/nrf52_demo/board.h
@@ -17,20 +17,5 @@
 #define BOARD_UART_TX_PIN	20
 #define BOARD_UART_RX_PIN	19
 
-/* Servos (some random pins, since we do not have nrf52 boards yet) */
-#define BOARD_SERVO_1_PORT	NRF_P0
-#define BOARD_SERVO_2_PORT	NRF_P0
-#define BOARD_SERVO_3_PORT	NRF_P0
-#define BOARD_SERVO_4_PORT	NRF_P0
-#define BOARD_SERVO_5_PORT	NRF_P0
-#define BOARD_SERVO_6_PORT	NRF_P0
-
-#define BOARD_SERVO_1_PIN	25
-#define BOARD_SERVO_2_PIN	26
-#define BOARD_SERVO_3_PIN	27
-#define BOARD_SERVO_4_PIN	28
-#define BOARD_SERVO_5_PIN	29
-#define BOARD_SERVO_6_PIN	30
-
 #endif
 
