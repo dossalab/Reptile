66d11330e5772d2015e0145eb000e37efba93fe4
Roman Luitko <dossalab@pm.me>
2020-10-24 23:31:31 +0300

    add mount event

--- core/main.c
+++ core/main.c
@@ -10,7 +10,23 @@
 #include <core/tasks.h>
 #include <core/temperature.h>
 #include <core/drive.h>
+#include <core/bus.h>
 #include <misc/log.h>
+#include <libs/fatfs/pff.h>
+
+#define PRINT_FILENAME	"PRINTME.GCO"
+
+static void on_drive_mounted(void *user)
+{
+	log_i("drive mounted, opening file...");
+
+	if (pf_open(PRINT_FILENAME) != FR_OK) {
+		log_e("unable to open file!");
+		return;
+	}
+
+	log_i("print file ready");
+}
 
 void main(void)
 {
@@ -18,11 +34,11 @@ void main(void)
 	log_i("booting reptile...");
 
 	task_system_init();
-	temperature_system_init();
 	drive_management_init();
+	temperature_system_init();
 
+	bus_subscribe(DRIVE_MOUNTED_MSG, on_drive_mounted);
 	irq_enable();
-
 	log_i("ready");
 
 	while (1) {
