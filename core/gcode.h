a4ae837d3c5a2d40cdc53e6e2efcca3c104352df
Roman Luitko <dossalab@pm.me>
2020-10-25 22:02:58 +0300

    add required gcode commands

--- core/gcode.h
+++ core/gcode.h
@@ -12,6 +12,23 @@
 
 #include <stddef.h>
 
+enum gcode_command {
+	GCODE_UNKNOWN,
+	GCODE_G1,	/* move (X, Y, Z, E - coordinates, F - feedrate) */
+	GCODE_G28,	/* home all axes */
+	GCODE_G20,	/* set units to inches */
+	GCODE_G21,	/* set units to millimeters */
+	GCODE_G90,	/* set absolute positioning */
+	GCODE_G91,	/* set relative positioning */
+	GCODE_G92,	/* set current position */
+	GCODE_M82,	/* set extruder to absolute mode */
+	GCODE_M84,	/* disarm motors */
+	GCODE_M104,	/* set extruder temperature (arg: S - temp) */
+	GCODE_M106,	/* fan on (S - PWM value (0...255)) */
+	GCODE_M107,	/* fan off */
+	GCODE_M109,	/* set extruder temperature and wait (arg: S - temp) */
+};
+
 struct gcode {
 
 };
